f231d528a0d4a9a9ebcfee016a05270f798d12b2
Divium <79859179+Divium-l@users.noreply.github.com>
2022-10-16 17:52:18 +0300

    added output into buffer, fixed addition and subtraction, division doesn't work

--- asm/AsmLab_2/main.cpp
+++ asm/AsmLab_2/main.cpp
@@ -19,8 +19,7 @@ int main() {
 	for (int i = 0; i++; i < DataSize) {
 		Memo[i] = NULL;
 	}
-
-	// ((25/c) - d + 2)/(b + a^2 - 1) 
+	
 	// (c - d + 2)/(b + a - 1) 
 	long long a = 0x1'527D'27F2; //5'678'901'234;
 	long long b = 0x1'94A8'1B79; //6'789'012'345;
@@ -28,7 +27,6 @@ int main() {
 	long long d = 0x2'128E'0F87; //8'901'234'567; 
 	long long e = 0x2'192D'7B4E; //9'012'345'678;
 	long long f = 0x2'5B67'B115; //10'123'456'789;
-	long long result = 0;
 	
 	__asm {
 		pushad
@@ -43,22 +41,35 @@ int main() {
 		SUB EDX, ECX
 		SBB EAX, EBX
 		// c - d + 2
-		SUB EDX, 2
-		SBB EAX, 0
+		ADD EDX, 2
+		ADC EAX, 0
+		LEA ESI, Memo
+		MOV [ESI], EDX
+		MOV [ESI + 4], EAX
 
-		// EBX:EDI + EDX:ESI - 1 = EBX:EDI
+		// ECX:EBX + EDI:ESI - 1 = ECX:EBX
 		// b
-		MOV EBX, 0x94A8'1B79
-		MOV EDI, 0x1
+		MOV ECX, 0x94A8'1B79
+		MOV EBX, 0x1
 		// a
-		MOV EDX, 0x527D'27F2
+		MOV EDI, 0x527D'27F2
 		MOV ESI, 0x1
 		// a + b
-		ADD EBX, EDX
-		ADC EDI, ESI
+		ADD ECX, EDI
+		ADC EBX, ESI
 		// a + b - 1
-		SUB EBX, 1
-		SBB EDI, 0
+		SUB ECX, 1
+		SBB EBX, 0
+		LEA ESI, Memo
+		MOV[ESI + 16], ECX
+		MOV[ESI + 20], EBX
+
+		// EDX:EAX / EBX:EDI = EDX:EAX
+		/**
+		* EDX:EAX = -1'011'111'109 = 0xFFFF'FFFF'C3BB'AB3B
+		* EBX:EDI = 12'467'913'578 = 0X0000'0002'E725'436A
+		**/
+		 
 
 		popad
 	}
