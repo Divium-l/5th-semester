b1b2ca8dce1d54b5438d2674db1e8097e23e6a43
Divium <79859179+Divium-l@users.noreply.github.com>
2022-10-30 11:36:25 +0300

    fixed broken template

--- asm/lab_template.cpp
+++ asm/lab_template.cpp
@@ -1,23 +1,21 @@
-#include<iostream>
-#include<conio.h>
-#include<iomanip>
-#include<sstream>
+#include <iostream>
+#include <conio.h>
+#include <iomanip>
+#include <sstream>
 
-using namespace std;
+const int DATA_SIZE = 144;
 
-const int DataSize = 144;
-
-std::string IntToHex(int n) {
+std::string intToHex(int n) {
 	std::stringstream ss;
 	ss << std::hex << n;
 	return ss.str();
 }
 
 int main() {
-	unsigned char Memo[DataSize];
+	unsigned char data[DATA_SIZE];
     
-	for (int i = 0; i++; i < DataSize) { 
-		Memo[i] = NULL; 
+	for (int i = 0; i < DATA_SIZE; i++) { 
+		data[i] = 0; 
 	}
 
 	__asm {
@@ -28,12 +26,12 @@ int main() {
 		popad
 	}
 
-	for (int i = 0; i < DataSize; i++) {
+	for (int i = 0; i < DATA_SIZE; i++) {
 		if ((i % 16) == 0) {
-			cout << "\n" << setw(2) << i / 16 << ":";
+			std::cout << "\n" << setw(2) << i / 16 << ":";
 		}
 		else {
-			cout << " " << setw(2) << IntToHex(Memo[i - 1]);
+			cout << " " << setw(2) << intToHex(data[i - 1]);
 		}
 	}
 
