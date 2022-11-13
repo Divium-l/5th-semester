d379b50bb08a8cc42ec3f5a8588c086ebf841c28
Divium <79859179+Divium-l@users.noreply.github.com>
2022-11-14 01:04:49 +0300

    Completed ind 1

--- asm/individual_1/individual_1.cpp
+++ asm/individual_1/individual_1.cpp
@@ -1,51 +1,56 @@
 ﻿#include <iostream>
 #include <random>
 
-const int32_t MIN = 0;
-const int32_t MAX = 16;
+const int32_t MIN = -255;
+const int32_t MAX = 255;
 const uint32_t ARR_SIZE = 3;
 
-void randomizeSquareMatrix(int** arr, const int size);
-void randomizeArray(int* arr, const int size);
+int random();
+void randomizeArray(int arr[ARR_SIZE], const int size);
+void randomizeSquareMatrix(int arr[ARR_SIZE][ARR_SIZE], const int size);
 
 int main()
 {
-    // index = 0..2
+    int32_t a[ARR_SIZE] = { 1, 2, 3 };
+    int32_t b[ARR_SIZE][ARR_SIZE] = { {1, 2, 3}, {4, -5, 6}, {7, 8, 9} };
+    int32_t x = -10;
+    int32_t result = 0;
+    /*
+    randomizeArray(a, ARR_SIZE);
+    randomizeSquareMatrix(b, ARR_SIZE);
+    x = random();
+    */
+    int32_t res_sum_a = 0;
+    int32_t res_sum_b = 0;
+    int32_t res_mult_1 = 0;
+    int32_t res_mult_2 = 0;
+    bool overflow = 0;
+
     // result = X * ΣA_i + X^2 * ΣB_ij
-    // 2147483647
-    int a[ARR_SIZE] = {1, 2, 3 };
-    int b[ARR_SIZE][ARR_SIZE] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
-    int x = 10;
-    int result = 0;
-
-    int res_sum_a = 0;
-    int res_sum_b = 0;
-    int res_mult_1 = 0;
-    int res_mult_2 = 0;
-    int overflow = 0;
-
     __asm {
         pushad
-
         JMP _start
 
-        // func_arr_sum(uint* arrPtr, uint size) -> int sum
-        // func_arr_sum(uint* EAX   , uint ECX ) -> int EBX
-    func_arr_sum:
+    // func_arr_sum(uint* arrPtr, uint size) -> int sum
+    // func_arr_sum(uint* EAX   , uint ECX ) -> int EBX
+    _func_arr_sum:
         XOR EBX, EBX
 
     loop_func_sum:
         ADD EBX, [EAX]
+        JO _func_end
         ADD EAX, 4
         LOOP loop_func_sum
-
+    _func_end:
         ret
+    // func end
 
     _start:
         // ΣA_i
         LEA EAX, a
         MOV ECX, 3
-        CALL func_arr_sum
+        CALL _func_arr_sum
+        JO _end_overflow
         ///debug//////////////////
         LEA EAX, res_sum_a      //
         MOV [EAX], EBX          //
@@ -55,34 +60,38 @@ int main()
         LEA EAX, x
         MOV EAX, [EAX]
         IMUL EBX
+        JO _end_overflow
         ///debug//////////////////
         LEA ECX, res_mult_1     //
         MOV[ECX], EAX           //
         ///debug//////////////////
-        PUSH EAX
+        MOV ESI, EAX
 
         // ΣB_ij
         LEA EAX, b
         MOV ECX, 9
-        CALL func_arr_sum
+        CALL _func_arr_sum
+        JO _end_overflow
         ///debug//////////////////
-        LEA EAX, res_sum_b      //
-        MOV [EAX], EBX          //
+        LEA ECX, res_sum_b      //
+        MOV [ECX], EBX          //
         ///debug//////////////////
-        
+
         // X^2 * ΣB_ij
         LEA EAX, x
         MOV EAX, [EAX]
         IMUL EAX
+        JO _end_overflow
         IMUL EBX
+        JO _end_overflow
         ///debug//////////////////
         LEA ECX, res_mult_2     //
         MOV [ECX], EAX          //
         ///debug//////////////////
 
         // X * ΣA_i + X^2 * ΣB_ij
-        POP EBX
-        ADD EAX, EBX
+        ADD EAX, ESI
+        JO _end_overflow
 
         // output
         LEA ECX, result
@@ -111,16 +120,21 @@ int main()
     std::cout << "overflow = " << (overflow ? "true" : "false") << std::endl;
 }
 
-void randomizeSquareMatrix(int** arr, const int size) {
-    for (int i = 0; i < size; i++)
-        randomizeArray(arr[i], size);
-}
-
-void randomizeArray(int* arr, const int size) {
+int random() {
     std::random_device rd;
     std::mt19937 rng(rd());
     std::uniform_int_distribution<int32_t> uni(MIN, MAX);
 
+    return uni(rng);
+}
+
+void randomizeArray(int arr[ARR_SIZE], const int size) {
     for (int i = 0; i < size; i++)
-        arr[i] = uni(rng);
+        arr[i] = random();
+}
+
+void randomizeSquareMatrix(int arr[ARR_SIZE][ARR_SIZE], const int size) {
+    for (int i = 0; i < size; i++)
+        for (int j = 0; j < size; j++)
+            arr[i][j] = random();
 }
