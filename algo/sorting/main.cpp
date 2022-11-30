cd2ab8d671c92000231e4630864469507e71410e
Dmitriy Rodionov <79859179+Divium-l@users.noreply.github.com>
2022-11-30 17:36:19 +0300

    added sorting comparison

--- algo/sorting/main.cpp
+++ algo/sorting/main.cpp
@@ -1,26 +1,50 @@
 #include <iostream>
 #include <random>
+#include <thread>
+#include <chrono>
+#include <string>
+#include <climits>
 #include "sort.hpp"
 
 void printArray(const int* array, const size_t size);
 void randomizeArray(int* array, const size_t size);
 void copyArray(int* to, const int* from, const size_t size);
 
+const unsigned int SIZE = 100'000;
+int random_b[SIZE];
+int random_q[SIZE];
+int sorted_b[SIZE];
+int sorted_q[SIZE];
+
+void testSort(int* array, void (*sort)(int*, const unsigned long) ,std::string arrayType, std::string sortType) {
+    std::cout << "Sorting " << arrayType << " array using " << sortType << std::endl;
+    auto start = std::chrono::system_clock::now();
+    sort(array, SIZE);
+    auto end = std::chrono::system_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    std::cout << "Sorting comlete. " << elapsed.count() << "ms elapsed" << std::endl;
+}
+
 int main() {
-    int random_b[1000];
-    int random_q[1000];
+    // Initialization
+    for (int i = 0; i < SIZE; i++) {
+        sorted_b[i] = i;
+        sorted_q[i] = i;
+    }
+    randomizeArray(random_b, SIZE);
+    copyArray(random_q, random_b, SIZE);
 
-    int sorted_q[1000];
-    int sorted_q[1000];
+    // Quick sort | random array
+    testSort(random_q, sort::qsort, "random", "quick sort");
 
-    int a[] = {2, 3, 0, -1};
-    int b[] = {2, 3, 0, -1};
+    // Quick sort | sorted array
+    testSort(sorted_q, sort::qsort, "sorted", "quick sort");
 
-    sort::bsort(a, 4);
-    printArray(a, 4);
+    // Bubble sort | random array
+    testSort(random_b, sort::bsort, "random", "buuble sort");
 
-    sort::qsort(b, 4);
-    printArray(b, 4);
+    // Bubble sort | sorted array
+    testSort(sorted_b, sort::bsort, "sorted", "buuble sort");
 
 }
 
@@ -34,7 +58,7 @@ void printArray(const int* array, const size_t size) {
 void randomizeArray(int* array, const size_t size) {
     std::random_device rd;     
 	std::mt19937 rng(rd());    
-	std::uniform_int_distribution<int> uni(0, 100);
+	std::uniform_int_distribution<int> uni(INT_MIN, INT_MAX);
 
     for (int i = 0; i < size; i++)
         array[i] = uni(rng);
