5d91d73a3b0a8355b711292d16a0821f3c851679
Dmitriy Rodionov <79859179+Divium-l@users.noreply.github.com>
2022-12-01 12:15:02 +0300

    added iterations count

--- algo/sorting/main.cpp
+++ algo/sorting/main.cpp
@@ -6,23 +6,27 @@
 #include <climits>
 #include "sort.hpp"
 
+typedef unsigned long long u64;
+
 void printArray(const int* array, const size_t size);
 void randomizeArray(int* array, const size_t size);
 void copyArray(int* to, const int* from, const size_t size);
 
-const unsigned int SIZE = 100'000;
+const unsigned int SIZE = 1'000;
 int random_b[SIZE];
 int random_q[SIZE];
 int sorted_b[SIZE];
 int sorted_q[SIZE];
 
-void testSort(int* array, void (*sort)(int*, const unsigned long) ,std::string arrayType, std::string sortType) {
+void testSort(int* array, void (*sort)(int*, const unsigned long) ,std::string arrayType, std::string sortType, u64 &iterations) {
+    iterations = 0;
     std::cout << "Sorting " << arrayType << " array using " << sortType << std::endl;
     auto start = std::chrono::system_clock::now();
     sort(array, SIZE);
     auto end = std::chrono::system_clock::now();
     auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
     std::cout << "Sorting comlete. " << elapsed.count() << "ms elapsed" << std::endl;
+    std::cout << iterations << " iterations." << std::endl << std::endl;
 }
 
 int main() {
@@ -35,16 +39,16 @@ int main() {
     copyArray(random_q, random_b, SIZE);
 
     // Quick sort | random array
-    testSort(random_q, sort::qsort, "random", "quick sort");
+    testSort(random_q, sort::qsort, "random", "quick sort", sort::qIterations);
 
     // Quick sort | sorted array
-    testSort(sorted_q, sort::qsort, "sorted", "quick sort");
+    testSort(sorted_q, sort::qsort, "sorted", "quick sort", sort::qIterations);
 
     // Bubble sort | random array
-    testSort(random_b, sort::bsort, "random", "buuble sort");
+    testSort(random_b, sort::bsort, "random", "buuble sort", sort::bIterations);
 
     // Bubble sort | sorted array
-    testSort(sorted_b, sort::bsort, "sorted", "buuble sort");
+    testSort(sorted_b, sort::bsort, "sorted", "buuble sort", sort::bIterations);
 
 }
 
--- algo/sorting/sort.cpp
+++ algo/sorting/sort.cpp
@@ -6,37 +6,52 @@ namespace sort {
             b = temp;
         }
 
+        unsigned long long bIterations;
+        unsigned long long qIterations;
+
         template<typename T> void _qsort(T* array, int low, int high) {
             int i = low;
             int j = high;
             T pivot = array[(i + j) / 2];
 
             while (i < j) {
-                while (array[i] < pivot)
+                while (array[i] < pivot) {
                     i++;
-                while (array[j] > pivot)
+                    qIterations++;
+                }
+
+                while (array[j] > pivot) {
                     j--;
+                    qIterations++;
+                }
 
                 if (i <= j) {
                     swap(array[i], array[j]);
                     i++;
                     j--;
+                    qIterations++;
                 }
             }
 
-            if (j > low)
+            if (j > low) {
                 _qsort(array, low, j);
+                qIterations++;
+            }
 
-            if (i < high)
+            if (i < high) {
                 _qsort(array, i, high);
+                qIterations++;
+            }
         }
     }
 
     template<typename T> void bsort(T* array, const unsigned long size) {
         for (int i = 0; i < size; i++)
-            for (int j = i + 1; j < size; j++)
+            for (int j = i + 1; j < size; j++) {
+                bIterations++;
                 if (array[i] > array[j])
                     swap(array[i], array[j]);
+            }
     }
 
     template<typename T> void qsort(T* array, const unsigned long size) {
