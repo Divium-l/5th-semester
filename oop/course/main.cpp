0885e9c67e1db8190bc0c5c8289864cc69b50497
Dmitriy Rodionov <79859179+Divium-l@users.noreply.github.com>
2022-12-01 11:58:35 +0300

    added menu

--- oop/course/main.cpp
+++ oop/course/main.cpp
@@ -3,19 +3,94 @@
 
 const int HOUR = 1'000 * 60 * 60;
 
+void init(Cinema &cinema);
+void showMenu();
+void showAll(Cinema &cinema);
+void showActive(Cinema &cinema);
+void serialize(Cinema &cinema);
+void deserialize(Cinema &cinema);
+
 int main(int argc, char** argv) {
-    Session session = Session(1, 1, "movie 1", 111, TimeRange(std::time(0) - 2 * HOUR, std::time(0) + 1 * HOUR));
-    Session session2 = Session(2, 2, "movie 2", 222, TimeRange());
-    Session session3 = Session(3, 3, "movie 3", 123, TimeRange(std::time(0), std::time(0) + 3 * HOUR));
+    auto cinema = Cinema();
+    char answer = '0';
+    init(cinema);
 
-    Cinema cinema = Cinema();
-    cinema.addSession(session);
-    cinema.addSession(session2);
-    auto active = cinema.getActiveSessions();
+    while (answer != 'e') {
+        system("clear");
+        showMenu();
+        std::cin >> answer;
 
-    std::cout << std::time(0) << std::endl; 
-    for (Session session : active) 
-        std::cout << "Active sessions: " << session.toString() << std::endl;
+        switch (answer) {
+            case '1':
+                showAll(cinema);
+                break;
+            case '2':
+                showActive(cinema);
+                break;
+            case '3':
+                //add
+                break;
+            case '4':
+                serialize(cinema);
+                break;
+            case '5':
+                deserialize(cinema);
+                break;
+            default:
+                break;
+        }
+        if (answer != 'e') {
+            std::cout << "Press Enter to continue" << std::endl;
+            std::cin.ignore(2147483647, '\n');
+            std::cin.get();
+        }
+    }
 
     return EXIT_SUCCESS;
+}
+
+void init(Cinema &cinema) {
+    Session session = Session(1, 1, "movie_1", 111, TimeRange(std::time(0) - 2 * HOUR, std::time(0) + 1 * HOUR));
+    Session session2 = Session(2, 2, "movie_2", 222, TimeRange());
+    Session session3 = Session(3, 3, "movie_3", 123, TimeRange(std::time(0), std::time(0) + 3 * HOUR));
+
+    cinema.addSession(session);
+    cinema.addSession(session2);
+    cinema.addSession(session3);
+}
+
+void showMenu() {
+    std::cout << "1. Show all sessions" << std::endl
+        << "2. Show active" << std::endl
+        << "3. Add session" << std::endl
+        << "4. Save to file" << std::endl
+        << "5. Read from file" << std::endl
+        << "e. Exit" << std::endl
+        << "option: ";
+}
+
+void showAll(Cinema &cinema) {
+    std::cout << "All sessions:" << std::endl;
+    for (Session session : cinema.getAllSessions()) 
+        std::cout << session.toString() << std::endl;
+}
+
+void showActive(Cinema &cinema) {
+    std::cout << "Active sessions:" << std::endl;
+    for (Session session : cinema.getActiveSessions()) 
+        std::cout << session.toString() << std::endl;
+}
+
+void serialize(Cinema &cinema) {
+    std::string path;
+    std::cout << "Enter file name: ";
+    std::cin >> path;
+    cinema.serialize(path);
+}
+
+void deserialize(Cinema &cinema) {
+    std::string path;
+    std::cout << "Enter file name: ";
+    std::cin >> path;
+    cinema.deserialize(path);
 }
